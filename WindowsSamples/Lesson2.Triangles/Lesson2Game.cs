8a797426ccb74118dd6cd4c2e9d9ec9ef99e3c0a
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2019-05-12 19:01:45 +0200

    Improve Lesson2.Triangles sample

--- WindowsSamples/Lesson2.Triangles/Lesson2Game.cs
+++ WindowsSamples/Lesson2.Triangles/Lesson2Game.cs
@@ -1,8 +1,4 @@
-﻿using System.IO;
-using System.Runtime.InteropServices;
-using BasicMaths;
-using JeremyAnsel.DirectX.D3D11;
-using JeremyAnsel.DirectX.Dxgi;
+﻿using JeremyAnsel.DirectX.D3D11;
 using JeremyAnsel.DirectX.GameWindow;
 using JeremyAnsel.DirectX.Window;
 
@@ -10,33 +6,7 @@ namespace Lesson2.Triangles
 {
     class Lesson2Game : GameWindowBase
     {
-#if DEBUG
-        private const string ShadersDirectory = "../../../Lesson2.Triangles.Shaders/Data/Debug/";
-#else
-        private const string ShadersDirectory = "../../../Lesson2.Triangles.Shaders/Data/Release/";
-#endif
-
-        private D3D11VertexShader vertexShader;
-
-        private D3D11InputLayout inputLayout;
-
-        private D3D11PixelShader pixelShader;
-
-        private D3D11Buffer vertexBuffer;
-
-        private D3D11Buffer indexBuffer;
-
-        private Float2[] triangleVertices = new Float2[]
-        {
-            new Float2(-0.5f, -0.5f),
-            new Float2( 0.0f,  0.5f),
-            new Float2( 0.5f, -0.5f),
-        };
-
-        private ushort[] triangleIndices = new ushort[]
-        {
-            0, 1, 2,
-        };
+        private MainGameComponent mainGameComponent;
 
         public Lesson2Game()
         {
@@ -44,6 +14,17 @@ namespace Lesson2.Triangles
 
         protected override void Init()
         {
+            this.mainGameComponent = new MainGameComponent();
+
+            D3D11FeatureLevel minimalFeatureLevel = this.RequestedD3DFeatureLevel;
+
+            if (this.mainGameComponent.MinimalFeatureLevel > minimalFeatureLevel)
+            {
+                minimalFeatureLevel = this.mainGameComponent.MinimalFeatureLevel;
+            }
+
+            this.RequestedD3DFeatureLevel = minimalFeatureLevel;
+
             base.Init();
         }
 
@@ -51,83 +32,40 @@ namespace Lesson2.Triangles
         {
             base.CreateDeviceDependentResources();
 
-            byte[] vertexShaderBytecode = File.ReadAllBytes(Lesson2Game.ShadersDirectory + "Triangles.VertexShader.cso");
-            this.vertexShader = this.DeviceResources.D3DDevice.CreateVertexShader(vertexShaderBytecode, null);
-
-            D3D11InputElementDesc[] basicVertexLayoutDesc = new D3D11InputElementDesc[]
-            {
-                new D3D11InputElementDesc
-                {
-                    SemanticName = "POSITION",
-                    SemanticIndex = 0,
-                    Format = DxgiFormat.R32G32Float,
-                    InputSlot = 0,
-                    AlignedByteOffset = 0,
-                    InputSlotClass = D3D11InputClassification.PerVertexData,
-                    InstanceDataStepRate = 0
-                }
-            };
-
-            this.inputLayout = this.DeviceResources.D3DDevice.CreateInputLayout(basicVertexLayoutDesc, vertexShaderBytecode);
-
-            byte[] pixelShaderBytecode = File.ReadAllBytes(Lesson2Game.ShadersDirectory + "Triangles.PixelShader.cso");
-            this.pixelShader = this.DeviceResources.D3DDevice.CreatePixelShader(pixelShaderBytecode, null);
-
-            var vertexBufferDesc = D3D11BufferDesc.From(triangleVertices, D3D11BindOptions.VertexBuffer);
-            this.vertexBuffer = this.DeviceResources.D3DDevice.CreateBuffer(vertexBufferDesc, triangleVertices, 0, 0);
-
-            var indexBufferDesc = D3D11BufferDesc.From(triangleIndices, D3D11BindOptions.IndexBuffer);
-            this.indexBuffer = this.DeviceResources.D3DDevice.CreateBuffer(indexBufferDesc, triangleIndices, 0, 0);
+            this.mainGameComponent.CreateDeviceDependentResources(this.DeviceResources);
         }
 
         protected override void ReleaseDeviceDependentResources()
         {
             base.ReleaseDeviceDependentResources();
 
-            D3D11Utils.DisposeAndNull(ref this.vertexShader);
-            D3D11Utils.DisposeAndNull(ref this.inputLayout);
-            D3D11Utils.DisposeAndNull(ref this.pixelShader);
-            D3D11Utils.DisposeAndNull(ref this.vertexBuffer);
-            D3D11Utils.DisposeAndNull(ref this.indexBuffer);
+            this.mainGameComponent.ReleaseDeviceDependentResources();
         }
 
         protected override void CreateWindowSizeDependentResources()
         {
             base.CreateWindowSizeDependentResources();
+
+            this.mainGameComponent.CreateWindowSizeDependentResources();
         }
 
         protected override void ReleaseWindowSizeDependentResources()
         {
             base.ReleaseWindowSizeDependentResources();
+
+            this.mainGameComponent.ReleaseWindowSizeDependentResources();
         }
 
         protected override void Update()
         {
             base.Update();
+
+            this.mainGameComponent.Update(this.Timer);
         }
 
         protected override void Render()
         {
-            var context = DeviceResources.D3DContext;
-
-            context.OutputMergerSetRenderTargets(new[] { this.DeviceResources.D3DRenderTargetView }, null);
-            context.ClearRenderTargetView(this.DeviceResources.D3DRenderTargetView, new float[] { 0.071f, 0.04f, 0.561f, 1.0f });
-
-            context.InputAssemblerSetInputLayout(this.inputLayout);
-
-            context.InputAssemblerSetVertexBuffers(
-                0,
-                new[] { this.vertexBuffer },
-                new uint[] { (uint)Marshal.SizeOf(typeof(Float2)) },
-                new uint[] { 0 });
-
-            context.InputAssemblerSetIndexBuffer(this.indexBuffer, DxgiFormat.R16UInt, 0);
-            context.InputAssemblerSetPrimitiveTopology(D3D11PrimitiveTopology.TriangleList);
-
-            context.VertexShaderSetShader(this.vertexShader, null);
-            context.PixelShaderSetShader(this.pixelShader, null);
-
-            context.DrawIndexed((uint)this.triangleIndices.Length, 0, 0);
+            this.mainGameComponent.Render();
         }
 
         protected override void OnKeyboardEvent(VirtualKey key, int repeatCount, bool wasDown, bool isDown)
---
+++ WindowsSamples/Lesson2.Triangles/MainGameComponent.cs
@@ -0,0 +1,132 @@
+﻿using BasicMaths;
+using JeremyAnsel.DirectX.D3D11;
+using JeremyAnsel.DirectX.Dxgi;
+using JeremyAnsel.DirectX.GameWindow;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Lesson2.Triangles
+{
+    class MainGameComponent : IGameComponent
+    {
+        private DeviceResources deviceResources;
+
+#if DEBUG
+        private const string ShadersDirectory = "../../../Lesson2.Triangles.Shaders/Data/Debug/";
+#else
+        private const string ShadersDirectory = "../../../Lesson2.Triangles.Shaders/Data/Release/";
+#endif
+
+        private D3D11VertexShader vertexShader;
+
+        private D3D11InputLayout inputLayout;
+
+        private D3D11PixelShader pixelShader;
+
+        private D3D11Buffer vertexBuffer;
+
+        private D3D11Buffer indexBuffer;
+
+        private Float2[] triangleVertices = new Float2[]
+        {
+            new Float2(-0.5f, -0.5f),
+            new Float2( 0.0f,  0.5f),
+            new Float2( 0.5f, -0.5f),
+        };
+
+        private ushort[] triangleIndices = new ushort[]
+        {
+            0, 1, 2,
+        };
+
+        public MainGameComponent()
+        {
+        }
+
+        public D3D11FeatureLevel MinimalFeatureLevel
+        {
+            get
+            {
+                return D3D11FeatureLevel.FeatureLevel91;
+            }
+        }
+
+        public void CreateDeviceDependentResources(DeviceResources resources)
+        {
+            this.deviceResources = resources;
+
+            byte[] vertexShaderBytecode = File.ReadAllBytes(MainGameComponent.ShadersDirectory + "Triangles.VertexShader.cso");
+            this.vertexShader = this.deviceResources.D3DDevice.CreateVertexShader(vertexShaderBytecode, null);
+
+            D3D11InputElementDesc[] basicVertexLayoutDesc = new D3D11InputElementDesc[]
+            {
+                new D3D11InputElementDesc
+                {
+                    SemanticName = "POSITION",
+                    SemanticIndex = 0,
+                    Format = DxgiFormat.R32G32Float,
+                    InputSlot = 0,
+                    AlignedByteOffset = 0,
+                    InputSlotClass = D3D11InputClassification.PerVertexData,
+                    InstanceDataStepRate = 0
+                }
+            };
+
+            this.inputLayout = this.deviceResources.D3DDevice.CreateInputLayout(basicVertexLayoutDesc, vertexShaderBytecode);
+
+            byte[] pixelShaderBytecode = File.ReadAllBytes(MainGameComponent.ShadersDirectory + "Triangles.PixelShader.cso");
+            this.pixelShader = this.deviceResources.D3DDevice.CreatePixelShader(pixelShaderBytecode, null);
+
+            var vertexBufferDesc = D3D11BufferDesc.From(triangleVertices, D3D11BindOptions.VertexBuffer);
+            this.vertexBuffer = this.deviceResources.D3DDevice.CreateBuffer(vertexBufferDesc, triangleVertices, 0, 0);
+
+            var indexBufferDesc = D3D11BufferDesc.From(triangleIndices, D3D11BindOptions.IndexBuffer);
+            this.indexBuffer = this.deviceResources.D3DDevice.CreateBuffer(indexBufferDesc, triangleIndices, 0, 0);
+        }
+
+        public void ReleaseDeviceDependentResources()
+        {
+            D3D11Utils.DisposeAndNull(ref this.vertexShader);
+            D3D11Utils.DisposeAndNull(ref this.inputLayout);
+            D3D11Utils.DisposeAndNull(ref this.pixelShader);
+            D3D11Utils.DisposeAndNull(ref this.vertexBuffer);
+            D3D11Utils.DisposeAndNull(ref this.indexBuffer);
+        }
+
+        public void CreateWindowSizeDependentResources()
+        {
+        }
+
+        public void ReleaseWindowSizeDependentResources()
+        {
+        }
+
+        public void Update(StepTimer timer)
+        {
+        }
+
+        public void Render()
+        {
+            var context = this.deviceResources.D3DContext;
+
+            context.OutputMergerSetRenderTargets(new[] { this.deviceResources.D3DRenderTargetView }, null);
+            context.ClearRenderTargetView(this.deviceResources.D3DRenderTargetView, new float[] { 0.071f, 0.04f, 0.561f, 1.0f });
+
+            context.InputAssemblerSetInputLayout(this.inputLayout);
+
+            context.InputAssemblerSetVertexBuffers(
+                0,
+                new[] { this.vertexBuffer },
+                new uint[] { (uint)Marshal.SizeOf(typeof(Float2)) },
+                new uint[] { 0 });
+
+            context.InputAssemblerSetIndexBuffer(this.indexBuffer, DxgiFormat.R16UInt, 0);
+            context.InputAssemblerSetPrimitiveTopology(D3D11PrimitiveTopology.TriangleList);
+
+            context.VertexShaderSetShader(this.vertexShader, null);
+            context.PixelShaderSetShader(this.pixelShader, null);
+
+            context.DrawIndexed((uint)this.triangleIndices.Length, 0, 0);
+        }
+    }
+}
